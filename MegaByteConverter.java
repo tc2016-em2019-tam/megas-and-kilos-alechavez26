7204837e92489c9ecbb8a842b0818134d0ba9a18
Alejandra Chávez Cruz <42187730+alechavez26@users.noreply.github.com>
2019-01-21 00:54:12 -0600

    Add files via upload

---
+++ MegaByteConverter.java
@@ -0,0 +1,27 @@
+﻿package com.company;
+
+public class MegabyteConverter {
+    private int kilobyte;
+
+
+    public int getKilobyte() {
+        return kilobyte;
+    }
+
+    public void setKilobyte(int kilobyte) throws Exception {
+
+        if (kilobyte<0) throw new Exception("You can't input negative numbers");
+        this.kilobyte = kilobyte;
+    }
+
+    public static void PrintMegaBytesAndKiloBytes(int kilobyte){
+        int kbyte = kilobyte/1024;
+        int remaining = kilobyte%1024;
+
+        if (remaining>0) {
+            System.out.println(kilobyte+" KB = "+kbyte+" MB and "+remaining+" KB");
+        } else {
+            System.out.println(kilobyte+" KB = "+kbyte+" MB");
+        }
+    }
+}
